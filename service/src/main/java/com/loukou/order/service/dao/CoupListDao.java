32e7749f23ad8dd00c70c36b9f27dd144fb89c70
陈海强 <chenhaiqiang@loukou.com>
2015-07-09 16:37:49 +0800

    fix conflict

--- service/src/main/java/com/loukou/order/service/dao/CoupListDao.java
+++ service/src/main/java/com/loukou/order/service/dao/CoupListDao.java
@@ -10,6 +10,7 @@ import com.loukou.order.service.entity.CoupList;
 
 public interface CoupListDao extends CrudRepository<CoupList, Integer>{
 
+//<<<<<<< HEAD
 	List<CoupList> findByUserIdAndIssueAndIschecked(int userId, int issue, int ischecked);
 
 	@Query("select cl from CoupList cl where userId = ?1 and ischecked=0 and issue=1 and begintime<=NOW() and endtime>=NOW() "
@@ -17,4 +18,8 @@ public interface CoupListDao extends CrudRepository<CoupList, Integer>{
 	List<CoupList> getCoupLists(int userId);
 
 	
+//=======
+//	@Query("SELECT c FROM CoupList WHERE userId = ?1 AND issure=1 AND ischecked=0 AND begintime<=NOW() AND endtime>=NOW()")
+//	List<CoupList> getValidCoups(int userId);
+//>>>>>>> for merge
 }
