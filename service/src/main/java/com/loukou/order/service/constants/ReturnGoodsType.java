f486e0fc8a962f482a9552a2b9c52b5e23f458fa
liyafei <liyafei@loukou.com>
2015-07-30 17:29:19 +0800

    add cancel order

---
+++ service/src/main/java/com/loukou/order/service/constants/ReturnGoodsType.java
@@ -0,0 +1,9 @@
+package com.loukou.order.service.constants;
+
+public class ReturnGoodsType {
+    //0商品订单1服务订单2jiazheng
+	public final static int GOODS = 0;
+	public final static int SERVICE = 1;
+	public final static int JIAZHENG = 2;
+	
+}
