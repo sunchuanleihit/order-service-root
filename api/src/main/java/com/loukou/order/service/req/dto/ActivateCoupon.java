a9bc1fc7cf74354b20030276b83deef4b06e9152
liyafei <liyafei@loukou.com>
2015-08-12 17:20:23 +0800

    add activateCoupon

---
+++ api/src/main/java/com/loukou/order/service/req/dto/ActivateCoupon.java
@@ -0,0 +1,43 @@
+package com.loukou.order.service.req.dto;
+
+import java.io.Serializable;
+
+public class ActivateCoupon implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 515391985030940343L;
+
+	private int userId;
+	
+	private String commoncode;// 公有券券码
+	
+	private String openId;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getCommoncode() {
+		return commoncode;
+	}
+
+	public void setCommoncode(String commoncode) {
+		this.commoncode = commoncode;
+	}
+
+	public String getOpenId() {
+		return openId;
+	}
+
+	public void setOpenId(String openId) {
+		this.openId = openId;
+	}
+	
+	
+}
